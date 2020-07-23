bd1b65efa3e5c691c5e81ec6a596e14f018e80f9
rk220217 <rk220217@gmail.com>
2020-07-23 21:09:58 +0530

    last commit

---
+++ precache-manifest.b9befdef3eaddd3b94c8beefce90d838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f64415f77b8519f9036",
+    "url": "/css/app.bd6e3d0c.css"
+  },
+  {
+    "revision": "0fe88ee54f6d35ee7639",
+    "url": "/css/chunk-vendors.8cb943aa.css"
+  },
+  {
+    "revision": "65404027b2baa0cea6c00835e0796c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f64415f77b8519f9036",
+    "url": "/js/app.11a8bb15.js"
+  },
+  {
+    "revision": "0fe88ee54f6d35ee7639",
+    "url": "/js/chunk-vendors.79f97f67.js"
+  },
+  {
+    "revision": "eda157c01917cca87d64f06a79473985",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ precache-manifest.e872b3f0ea403a3397f69ba110cf0e35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d23a7f56421a8e588f",
+    "url": "/css/app.b2e1d747.css"
+  },
+  {
+    "revision": "5dab3fc785bf2eba0ab4",
+    "url": "/css/chunk-vendors.b8ce8fde.css"
+  },
+  {
+    "revision": "84421e5490e40d26b037d33d9f3daa3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d23a7f56421a8e588f",
+    "url": "/js/app.afc35670.js"
+  },
+  {
+    "revision": "5dab3fc785bf2eba0ab4",
+    "url": "/js/chunk-vendors.57dc2ac0.js"
+  },
+  {
+    "revision": "eda157c01917cca87d64f06a79473985",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
